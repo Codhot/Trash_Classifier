5cec644a5b39653aeb7f301320dfd187470b90c0
Codhot <162153685+Codhot@users.noreply.github.com>
2025-06-20 13:59:48 +0700

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,4 +4,4 @@
 # bind = f"0.0.0.0:{os.getenv('PORT', '5000')}" 
 
 # workers = 1 # Sesuaikan jumlah worker sesuai kebutuhan dan resource Railway kamu
-# timeout = 120 # Timeout untuk request, bisa disesuaikan
+# timeout = 120 # Timeout untuk request, bisa disesuaikan   
